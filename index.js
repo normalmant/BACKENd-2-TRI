ddec8855275d0891c05505c7ff0fb0165c7a64a0
Renato <114404480+normalmant@users.noreply.github.com>
2026-06-22 08:53:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -39,12 +39,66 @@ app.get("/clientes",(req,res)=>{
     }
 });
 
+
+
+
+
+
+app.get("/clientes/:cpf",(req,res)=>{
+
+    const cpf= req.params.cpf
+
+
+    try{
+        const bd=JSON.parse(fs.readFileSync("bd.json","utf8"))
+
+        const cliente= bd.find((cliente)=> cliente.cpf==cpf)
+        if(!cliente){
+            return res.status(404).json({erro: "Cliente não existe no Banco de Dados"})
+        }
+        res.status(200).json({ respota:cliente})
+    }catch(erro){
+        res.status(500).json({erro:erro.message})
+    }
+});
+
+// TESTE 
+app.get("/clientes/nome/:nome",(req,res)=>{
+
+    const nome= req.params.nome
+
+    try{
+        const bd=JSON.parse(fs.readFileSync("bd.json","utf8"))
+
+        const cliente= bd.find((cliente)=> cliente.nome==nome)
+        if(!cliente){
+            return res.status(404).json({erro: "Cliente não existe no Banco de Dados"})
+        }
+        res.status(200).json({ respota:cliente})
+    }catch(erro){
+        res.status(500).json({erro:erro.message})
+    }
+});
+
+
+
+
+
+
+
+
+
 app.get("/perfil", (req, res) => {
     res.json({
         nome: "Carlos Alberto",
         idade: "72 anos"
     });
 });
+
+
+
+
+
 
 app.get("/ola", (req, res) => {
     res.json({
